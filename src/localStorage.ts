af933f2bfa8b17524c2cd6f7911a1fe072ec24e5
pikax <david-181@hotmail.com>
2019-11-02 12:36:13 +0000

    WIP: implemented localstorage tests

--- src/localStorage.ts
+++ src/localStorage.ts
@@ -1,21 +1,22 @@
 import { Ref, ref, watch } from "@vue/composition-api";
-import { RefTyped, unwrap, wrap } from "./utils";
+import { RefTyped, wrap } from "./utils";
 import { debounce } from "./debounce";
 
 // used to store all the instances of weakMap
-const weakMap = new WeakMap<String, Ref<object | undefined>>();
+const keyedMap = new Map<string, object>();
+const weakMap = new WeakMap<object, Ref<object | undefined>>();
 export type LocalStorageTyped<T> = string;
 
 export interface LocalStorageReturn<T> {
   storage: Ref<T>;
 
   /**
-   * Removes current item from the store
+   * @description Removes current item from the store
    */
   remove: () => void;
 
   /**
-   * Clears all tracked localStorage items
+   * @description Clears all tracked localStorage items
    */
   clear: () => void;
 }
@@ -25,15 +26,21 @@ export function useLocalStorage<T = any>(
   key: LocalStorageTyped<T> | string,
   defaultValue?: RefTyped<T>
 ): LocalStorageReturn<T>;
-export function useLocalStorage(key?: any, defaultValue?: any) {
+export function useLocalStorage(key: string, defaultValue?: any) {
   let lazy = false;
+  let k = keyedMap.get(key);
   const json = localStorage.getItem(key);
-  const storage = (!!defaultValue && wrap(defaultValue)) || ref(null);
-  if (json) {
+  const storage =
+    (k && weakMap.get(k)) ||
+    (!!defaultValue && wrap(defaultValue)) ||
+    ref(null);
+
+  if (json && !k) {
     try {
       storage.value = JSON.parse(json);
       lazy = false;
     } catch (e) {
+      /* istanbul ignore next */
       console.warn(
         "[useLocalStorage] error parsing value from localStorage",
         key,
@@ -42,36 +49,50 @@ export function useLocalStorage(key?: any, defaultValue?: any) {
     }
   }
 
-  weakMap.set(key, storage);
+  // do not watch if we already created the instance
+  if (!k) {
+    k = {};
+    keyedMap.set(key, k);
+    weakMap.set(k, storage);
 
-  const clear = () => {
-    for (let i = 0; i < localStorage.length; i++) {
-      const obj = weakMap.get(localStorage.key(i) as any);
-      if (!!obj) {
-        obj.value = undefined;
+    watch(
+      storage,
+      storage => {
+        if (storage === undefined) {
+          localStorage.removeItem(key);
+          return;
+        }
+        // do not overflow localStorage with updates nor keep doing stringify
+        debounce(
+          () => localStorage.setItem(key, JSON.stringify(storage)),
+          100
+        )();
+      },
+      {
+        deep: true,
+        lazy
       }
-    }
-  };
-
-  const remove = ()=> {
-      storage.value = undefined;
+    );
   }
 
-  watch(
-    storage,
-    storage => {
-      if (storage === undefined) {
-        localStorage.removeItem(key);
-        return;
+  const clear = () => {
+    keyedMap.forEach((v) => {
+      const obj = weakMap.get(v);
+      /* istanbul ignore else */
+      if (obj) {
+        obj.value = undefined;
       }
-      // do not overflow localStorage with updates nor keep doing stringify
-      debounce(() => localStorage.setItem(key, JSON.stringify(storage)), 300);
-    },
-    {
-      deep: true,
-      lazy
-    }
-  );
+      weakMap.delete(v);
+    });
+
+    keyedMap.clear();
+  };
+
+  const remove = () => {
+    keyedMap.delete(key);
+    weakMap.delete(k!);
+    storage.value = undefined;
+  };
 
   return {
     storage,
