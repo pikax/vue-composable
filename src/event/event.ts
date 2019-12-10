f94f636de0981aec2f173ea8e15f98b5b1742ca9
pikax <carlos@hypermob.co.uk>
2019-12-10 19:27:48 +0000

    types: fix Ref<Element> to use RefElement

--- src/event/event.ts
+++ src/event/event.ts
@@ -1,5 +1,5 @@
 import { Ref, isRef, onMounted, onUnmounted } from "@vue/composition-api";
-import { RefTyped , wrap} from "../utils";
+import { RefTyped , wrap, RefElement} from "../utils";
 
 export type RemoveEventFunction = () => void;
 
@@ -10,22 +10,22 @@ export function useEvent<K extends keyof WindowEventMap>(
   options?: boolean | AddEventListenerOptions
 ): RemoveEventFunction;
 export function useEvent<K extends keyof DocumentEventMap>(
-  el: RefTyped<Element>,
+  el: RefElement,
   name: K,
   listener: (this: Document, ev: DocumentEventMap[K]) => any,
   options?: boolean | AddEventListenerOptions
 ): RemoveEventFunction;
 export function useEvent(
-  el: RefTyped<Element> | RefTyped<Window>,
+  el: RefElement| RefTyped<Window>,
   name: string,
   listener: EventListenerOrEventListenerObject,
   options?: boolean | AddEventListenerOptions
 ): RemoveEventFunction {
   const element = wrap(el);
 
-  const remove = () => element.value.removeEventListener(name, listener);
+  const remove = () => element.value!.removeEventListener(name, listener);
 
-  onMounted(() => element.value.addEventListener(name, listener, options));
+  onMounted(() => element.value!.addEventListener(name, listener, options));
   onUnmounted(remove);
 
   return remove;
