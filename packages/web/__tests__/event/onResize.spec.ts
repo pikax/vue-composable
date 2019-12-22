f22625bf7b515091639f314eb0df5ddd23c90a90
pikax <david-181@hotmail.com>
2019-12-22 09:46:01 +0000

    test(web): fix resize tests to check for window.addEventListener

--- packages/web/__tests__/event/onResize.spec.ts
+++ packages/web/__tests__/event/onResize.spec.ts
@@ -3,16 +3,30 @@ import { useOnResize, ResizeResult } from "../../src/event/onResize";
 import { promisedTimeout } from "@vue-composable/core";
 
 describe("onResize", () => {
+  const windowEventSpy = jest.fn();
+  const windowEvent = window.addEventListener;
+  const windowRemoveEvent = window.removeEventListener;
+
+  beforeAll(() => {
+    window.addEventListener = windowEventSpy;
+  })
+  afterAll(() => {
+    window.addEventListener = windowEvent;
+    window.removeEventListener = windowRemoveEvent;
+  })
+
   it("should add the correct event", async () => {
     const element: Element = {
-      addEventListener: jest.fn().mockImplementation((name, listener) => {
-        expect(name).toBe("resize");
-        handler = listener;
-      }),
       removeEventListener: jest.fn(),
       clientHeight: 0,
       clientWidth: 0
     } as any;
+
+    windowEventSpy.mockImplementation((name, listener) => {
+      expect(name).toBe("resize");
+      handler = listener;
+    });
+
     let handler: ((ev: Partial<MouseEvent>) => void) | undefined = undefined;
     let use: ResizeResult | undefined = undefined;
 
@@ -23,7 +37,7 @@ describe("onResize", () => {
       }
     }).$mount();
 
-    expect(element.addEventListener).toHaveBeenCalled();
+    expect(window.addEventListener).toHaveBeenCalled();
 
     expect(use).toMatchObject({
       height: { value: 0 },
@@ -45,11 +59,11 @@ describe("onResize", () => {
   it("should removeEventListener if `remove` is called", () => {
     const element: Element = {
       addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
       clientHeight: 0,
       clientWidth: 0
     } as any;
     let use: ResizeResult | undefined = undefined;
+    window.removeEventListener = jest.fn();
 
     new Vue({
       template: "<div></div>",
@@ -57,23 +71,23 @@ describe("onResize", () => {
         use = useOnResize(element);
       }
     }).$mount();
-    expect(element.removeEventListener).not.toHaveBeenCalled();
+    expect(window.removeEventListener).not.toHaveBeenCalled();
 
     use!.remove();
 
-    expect(element.removeEventListener).toHaveBeenCalled();
+    expect(window.removeEventListener).toHaveBeenCalled();
   });
 
   it("should debounce if wait is passed", async () => {
     const element: Element = {
-      addEventListener: jest.fn().mockImplementation((name, listener) => {
-        expect(name).toBe("resize");
-        handler = listener;
-      }),
       removeEventListener: jest.fn(),
       clientHeight: 0,
       clientWidth: 0
     } as any;
+    windowEventSpy.mockImplementation((name, listener) => {
+      expect(name).toBe("resize");
+      handler = listener;
+    });
     let use: ResizeResult | undefined = undefined;
     let handler: ((ev: Partial<MouseEvent>) => void) | undefined = undefined;
     const wait = 50;
@@ -84,7 +98,7 @@ describe("onResize", () => {
         use = useOnResize(element, wait);
       }
     }).$mount();
-    expect(element.addEventListener).toHaveBeenCalled();
+    expect(window.addEventListener).toHaveBeenCalled();
 
     for (let i = 0; i < 10; i++) {
       (element as any).clientHeight = 10 + i;
@@ -125,23 +139,23 @@ describe("onResize", () => {
         return useOnResize(element, options);
       }
     }).$mount();
-    expect(element.addEventListener).toHaveBeenCalledWith(
+    expect(window.addEventListener).toHaveBeenCalledWith(
       "resize",
       expect.any(Function),
       options
     );
   });
-  
+
   it("should pass options to the event listener and be debounced", async () => {
     const element: Element = {
-      addEventListener: jest.fn().mockImplementation((name, listener) => {
-        expect(name).toBe("resize");
-        handler = listener;
-      }),
       removeEventListener: jest.fn(),
       clientHeight: 0,
       clientWidth: 0
     } as any;
+    windowEventSpy.mockImplementation((name, listener) => {
+      expect(name).toBe("resize");
+      handler = listener;
+    });
     let use: ResizeResult | undefined = undefined;
     let handler: ((ev: Partial<MouseEvent>) => void) | undefined = undefined;
     const wait = 50;
@@ -155,7 +169,7 @@ describe("onResize", () => {
         use = useOnResize(element, options, wait);
       }
     }).$mount();
-    expect(element.addEventListener).toHaveBeenCalledWith(
+    expect(window.addEventListener).toHaveBeenCalledWith(
       "resize",
       expect.any(Function),
       options
