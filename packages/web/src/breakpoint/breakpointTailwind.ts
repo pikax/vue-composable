42b12be796f4145cf1f5931315bb1f552a60793e
pikax <carlos@hypermob.co.uk>
2020-02-17 12:32:14 +0000

    feat(web): improve tailwind breakpoints

--- packages/web/src/breakpoint/breakpointTailwind.ts
+++ packages/web/src/breakpoint/breakpointTailwind.ts
@@ -4,7 +4,7 @@ import {
   BreakpointObject,
   useBreakpoint
 } from "./breakpoint";
-import { isObject } from "@vue-composable/core";
+import { isArray, isString, isObject } from "@vue-composable/core";
 
 const BREAKPOINT_TAILWIND_KEY: InjectionKey<any> = Symbol(
   (__DEV__ && "TAILWIND_BREAKPOINTS") || ``
@@ -24,9 +24,24 @@ const defaultTailwindBreakpoint: BreakpointObject = ({
   xl: 1280
 } as DefaultTailwindBreakpoints) as any;
 
+interface TailwindScreenBreakpointRaw {
+  raw: string;
+}
+
+interface TailwindScreenBreakpointRange {
+  min?: string;
+  max?: string;
+}
+
+type TailwindScreen =
+  | TailwindScreenBreakpointRaw
+  | TailwindScreenBreakpointRange
+  | string
+  | Array<TailwindScreenBreakpointRange>;
+
 interface TailwindConfigEmpty {
   theme: {
-    screens: {} | undefined | null;
+    screens: TailwindScreen | Array<TailwindScreen> | undefined | null;
   };
 }
 
@@ -46,6 +61,44 @@ function isTailwind(t: any): t is TailwindConfig {
   return isObject(t.theme) && isObject(t.theme.screen);
 }
 
+function isRawScreen(t: any): t is TailwindScreenBreakpointRaw {
+  return isString(t.raw);
+}
+
+function isRangeScreen(t: any): t is TailwindScreenBreakpointRange {
+  return isString(t.min) || isString(t.max);
+}
+
+function screenRangeToBreakpoint(s: TailwindScreenBreakpointRange) {
+  const condition = [
+    s.max && `max-width: ${s.max}`,
+    s.min && `min-width: ${s.min}`
+  ].join(" and ");
+
+  return `(${condition})`;
+}
+
+function screenToBreakpoint(s: TailwindScreen): string[] {
+  const conditions: string[] = [];
+
+  if (isArray(s)) {
+    const bks = s.map(screenToBreakpoint);
+    for (let i = 0; i < bks.length; i++) {
+      const b = bks[i];
+      conditions.push(...b);
+    }
+  } else {
+    if (isRawScreen(s)) {
+      conditions.push(s.raw);
+    } else if (isRangeScreen(s)) {
+      conditions.push(screenRangeToBreakpoint(s));
+    } else {
+      conditions.push(`min-width: ${s}`);
+    }
+  }
+  return conditions;
+}
+
 export function setBreakpointTailwindCSS<T extends TailwindConfigEmpty>(
   tailwindConfig: T
 ): BreakpointReturn<ExtractTailwindScreens<T>>;
@@ -56,6 +109,15 @@ export function setBreakpointTailwindCSS<T extends BreakpointObject>(
   breakpoints: T | TailwindConfig
 ): BreakpointReturn<any> {
   const bk = isTailwind(breakpoints) ? breakpoints.theme.screens : breakpoints;
+
+  if (isTailwind(breakpoints)) {
+    for (const k in bk) {
+      if (bk.hasOwnProperty(k)) {
+        bk[k] = screenToBreakpoint(bk).join(" or ");
+      }
+    }
+  }
+
   const bp = useBreakpoint(bk);
   provide(BREAKPOINT_TAILWIND_KEY, bp);
   return bp;
