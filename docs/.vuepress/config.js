a76e725b060a6dd6ca4db69acaa5b2649bd13d4f
pikax <david-181@hotmail.com>
2020-02-20 14:42:25 +0000

    fix vuepress icons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,71 +29,6 @@ module.exports = {
       "link",
       {
         rel: "apple-touch-icon",
-        sizes: "57x57",
-        href: "/img/apple-icon-57x57.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "60x60",
-        href: "/img/apple-icon-60x60.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "72x72",
-        href: "/img/apple-icon-72x72.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "76x76",
-        href: "/img/apple-icon-76x76.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "114x114",
-        href: "/img/apple-icon-114x114.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "120x120",
-        href: "/img/apple-icon-120x120.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "144x144",
-        href: "/img/apple-icon-144x144.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "152x152",
-        href: "/img/apple-icon-152x152.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
         href: "/img/apple-touch-icon.png"
       }
     ],
@@ -113,15 +48,6 @@ module.exports = {
         type: "image/png",
         sizes: "32x32",
         href: "/img/favicon-32x32.png"
-      }
-    ],
-    [
-      "link",
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "96x96",
-        href: "/img/favicon-96x96.png"
       }
     ],
     [
