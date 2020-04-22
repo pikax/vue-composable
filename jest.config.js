f385a3cf07a033b6960c1efc7ce3a78f6f75e919
pikax <carlos@hypermob.co.uk>
2020-04-22 10:16:25 +0100

    build: fix tests

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   globals: {
     __DEV__: true,
     __VERSION__: pkg.version,
-    __VUE_2__: process.env.VUE === "2"
+    __VUE_2__: process.env.VUE === "2",
+    __COMMIT__: "none"
   },
   setupFiles: [
     "<rootDir>/__tests__/setupTest.js",
