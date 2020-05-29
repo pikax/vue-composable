1d8b28e29015418e6204ce6bd40afa56c55acaf2
Carlos Rodrigues <carlos@hypermob.co.uk>
2020-05-29 17:23:20 +0100

    feat(useIntlDateTimeFormat): add useIntlDateTimeFormat

---
+++ packages/vue-composable/src/intl/dateTimeFormat.ts
@@ -0,0 +1,96 @@
+import { computed, Ref } from '../api';
+import { intlDateFormatExtractArguments } from './_helper';
+import { RefTyped, unwrap } from '../utils';
+// import { DateTimeFormatOptions } from "./types";
+
+export type DateTimeFormatLocales =
+  | RefTyped<string>
+  | RefTyped<string[]>
+  | undefined;
+
+export type DateTimeFormatterFormat<T> = (
+  value: Readonly<RefTyped<Readonly<number | Date>>>,
+  overrideOpts?: RefTyped<Intl.DateTimeFormatOptions>,
+  overrideLocale?: RefTyped<DateTimeFormatLocales>
+) => T;
+
+export interface DateTimeFormatReturn {
+  formatter: Ref<Readonly<Intl.DateTimeFormat>>;
+
+  format: DateTimeFormatterFormat<Ref<Readonly<string>>>;
+  formatString: DateTimeFormatterFormat<string>;
+}
+
+export function useIntlDateTimeFormat(): DateTimeFormatReturn;
+
+export function useIntlDateTimeFormat(
+  locales: DateTimeFormatLocales
+): DateTimeFormatReturn;
+
+// export function useIntlDateTimeFormat(
+//   options: RefTyped<DateTimeFormatOptions>
+// ): DateTimeFormatReturn;
+
+export function useIntlDateTimeFormat(
+  options: RefTyped<Intl.DateTimeFormatOptions>
+): DateTimeFormatReturn;
+
+// export function useIntlDateTimeFormat(
+//   locales: DateTimeFormatLocales,
+//   options?: RefTyped<DateTimeFormatOptions | undefined>
+// ): DateTimeFormatReturn;
+
+export function useIntlDateTimeFormat(
+  locales: DateTimeFormatLocales,
+  options: RefTyped<Intl.DateTimeFormatOptions | undefined>
+): DateTimeFormatReturn;
+
+export function useIntlDateTimeFormat(
+  localesOptions?:
+    | DateTimeFormatLocales
+    // | RefTyped<DateTimeFormatOptions>
+    | RefTyped<Intl.DateTimeFormatOptions>,
+  opts?: any
+) {
+  const [locales, options] = intlDateFormatExtractArguments(
+    localesOptions as any,
+    opts
+  );
+
+  const formatter = computed(
+    () =>
+      new Intl.DateTimeFormat(
+        unwrap(locales as Ref<string | string[]> | undefined),
+        unwrap(options)
+      )
+  );
+
+  const formatString = (
+    value: RefTyped<number | Date>,
+    overrideOpts?: RefTyped<Intl.DateTimeFormatOptions>,
+    overrideLocale?: RefTyped<DateTimeFormatLocales>
+  ) => {
+    const f =
+      overrideOpts || overrideLocale
+        ? new Intl.DateTimeFormat(
+            unwrap(overrideLocale as RefTyped<string>) ||
+              unwrap(locales as Ref<string | string[]>),
+            { ...unwrap(options), ...unwrap(overrideOpts) }
+          )
+        : formatter.value;
+    return f.format(unwrap(value));
+  };
+
+  const format = (
+    value: RefTyped<number | Date>,
+    overrideOpts?: RefTyped<Intl.DateTimeFormatOptions>,
+    overrideLocale?: RefTyped<DateTimeFormatLocales>
+  ) => computed(() => formatString(value, overrideOpts, overrideLocale));
+
+  return {
+    format,
+    formatString,
+
+    formatter,
+  };
+}
--- packages/vue-composable/src/intl/index.ts
+++ packages/vue-composable/src/intl/index.ts
@@ -1,3 +1,4 @@
+export * from "./dateTimeFormat";
 export * from "./numberFormat";
 export * from "./currencyFormat";
 export * from "./types";
