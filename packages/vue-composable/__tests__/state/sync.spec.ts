8e8c452d0954877b592621fb755db4cb753ebe17
Carlos Rodrigues <carlos@hypermob.co.uk>
2020-06-08 08:00:10 +0100

    chore: more coverage

--- packages/vue-composable/__tests__/state/sync.spec.ts
+++ packages/vue-composable/__tests__/state/sync.spec.ts
@@ -1,5 +1,5 @@
 import { ref } from "../../src/api";
-import { useSync } from "../../src";
+import { useSync, wrap } from "../../src";
 import { nextTick } from "../utils";
 
 describe("sync", () => {
@@ -43,5 +43,45 @@ describe("sync", () => {
       { value: o },
       { value: o }
     ]);
+
+    const o2 = { c: 1 };
+    list.value[0].value = o2 as any;
+    await nextTick();
+
+    expect(list.value).toMatchObject([
+      { value: o2 },
+      { value: o2 },
+      { value: o2 }
+    ]);
+    expect(main.value).toMatchObject(o2);
+
+    const newValue = ref(1);
+    list.value.push(newValue as any);
+    await nextTick();
+    expect(newValue.value).toMatchObject(o2);
+  });
+
+  it("should receive ref<Array>", () => {
+    const main = ref({ a: 1 });
+
+    const others = [{ a: 2 }, { a: 10 }, { a: 1 }];
+
+    const list = useSync(main, ref(others.map(x => wrap(x))));
+
+    expect(list.value).toMatchObject([
+      { value: main.value },
+      { value: main.value },
+      { value: main.value }
+    ]);
+  });
+
+  it("should receive one element", () => {
+    const main = ref({ a: 1 });
+
+    const other = { a: 2 };
+
+    const list = useSync(main, other);
+
+    expect(list.value).toMatchObject([{ value: main.value }]);
   });
 });
