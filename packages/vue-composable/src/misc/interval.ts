0cf6cee4f2ce264443e810f2d51d95e55520921b
pikax <carlos@hypermob.co.uk>
2020-07-26 09:21:00 +0100

    chore: improve typings

--- packages/vue-composable/src/misc/interval.ts
+++ packages/vue-composable/src/misc/interval.ts
@@ -1,24 +1,62 @@
 import { onUnmounted } from "../api";
+import { isNumber } from "../utils";
 
-export interface UseIntervalReturn {
-  start(ms: number, ...args: any[]): number;
+export interface UseIntervalReturn<TArgs extends Array<any>> {
+  start(ms: number, ...args: TArgs): number;
+
   remove(): void;
 }
 
-export const useInterval = (
-  callback: (...args: any[]) => void,
+export interface UseIntervalReturnMs {
+  start(): number;
+}
+
+export interface UseIntervalReturnArgs<TArgs extends Array<any>> {
+  start(_: undefined, ...args: TArgs): number;
+}
+
+export function useInterval<TArgs extends Array<any>>(
+  callback: (...args: TArgs) => void,
+  ms?: false,
+  ...args: TArgs
+): UseIntervalReturn<TArgs>;
+
+export function useInterval<TArgs extends Array<any>>(
+  callback: (...args: TArgs) => void,
+  ms?: false
+): UseIntervalReturn<TArgs>;
+
+export function useInterval<TArgs extends Array<any>>(
+  callback: (...args: TArgs) => void,
   ms: number,
-  ...args: any[]
-): UseIntervalReturn => {
+  ...args: TArgs
+): UseIntervalReturn<TArgs> &
+  UseIntervalReturnMs &
+  UseIntervalReturnArgs<TArgs>;
+
+export function useInterval<TArgs extends Array<any>>(
+  callback: (...args: TArgs) => void,
+  ms?: number | false,
+  ...args: TArgs
+): UseIntervalReturn<TArgs> &
+  UseIntervalReturnMs &
+  UseIntervalReturnArgs<TArgs> {
   let intervalId: number;
 
   const start = (_ms?: number, ..._args: any[]) =>
-    (intervalId = setInterval(callback, _ms || ms, _args || args) as any);
+    (intervalId = setInterval(callback, _ms || ms || 10, _args || args) as any);
 
   const remove = () => clearInterval(intervalId);
 
-  start();
+  if (isNumber(ms)) {
+    start();
+  }
 
   onUnmounted(remove);
   return { remove, start };
-};
+}
+
+// TODO move this:  type checkings
+// useInterval((x: number) => {}).start();
+// useInterval((x: number) => {}, 100, 1).start();
+// useInterval(() => {}).start();
